2be32273a4b2f256e2ab2d2555c0d855902514e8
gennadykudryavtsev <gennadykudryavtsev@gmail.com>
2024-11-03 23:54:10 +0300

    5_8_spring_cache.patch

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -3,13 +3,14 @@ package ru.javawebinar.topjava;
 public class Profiles {
     public static final String
             JDBC = "jdbc",
-            JPA = "jpa";
+            JPA = "jpa",
+            DATAJPA = "datajpa";
 
-    public static final String REPOSITORY_IMPLEMENTATION = JPA;
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
 
     public static final String
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
-    public static final String ACTIVE_DB = POSTGRES_DB;
+    public static final String ACTIVE_DB = HSQL_DB;
 }
