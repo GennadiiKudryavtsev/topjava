c7c3f1fb86bbd8c6f7ec02ca9e6cc8c88187e360
gennadykudryavtsev <gennadykudryavtsev@gmail.com>
2025-12-18 14:40:13 +0300

    11_11_error_status.patch

--- src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.MessageSourceAccessor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -40,15 +39,9 @@ public class GlobalExceptionHandler {
 
         ModelAndView mav = new ModelAndView("exception",
                 Map.of("exception", rootCause, "message", ValidationUtil.getMessage(rootCause),
-                        "typeMessage", messageSourceAccessor.getMessage(ErrorType.APP_ERROR.getErrorCode()),
-                        "status", httpStatus));
-        mav.setStatus(httpStatus);
-
-        // Interceptor is not invoked, put userTo
-        AuthorizedUser authorizedUser = SecurityUtil.safeGet();
-        if (authorizedUser != null) {
-            mav.addObject("userTo", authorizedUser.getUserTo());
-        }
+                        "typeMessage", messageSourceAccessor.getMessage(errorType.getErrorCode()),
+                        "status", errorType.getStatus()));
+        mav.setStatus(errorType.getStatus());
         return mav;
     }
 }
